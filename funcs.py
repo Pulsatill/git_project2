f821e6ade7d3916eb14af034a963f61c7f7412ac
Pulsatill <arch-angel2002@yandex.ru>
2023-01-25 08:47:47 +0900

    use function for subtraction in main.py

--- funcs.py
+++ funcs.py
@@ -1,6 +1,6 @@
-def func_addition():
+def func_addition(x, z):
     print(x + z)
 
 
-def func_subtraction():
+def func_subtraction(x, z):
     print(x - z)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from funcs import func_addition
+from funcs import func_subtraction
 
 # назначение переменных
 x = 10
@@ -6,3 +7,6 @@ z = 5
 
 # вызов функции сложения
 func_addition(x, z)
+
+# вызов функции вычитания
+func_subtraction(x, z)
